789e3f6708bd0304b37926ed48b150c151a48963
Lakshay Choudhary <92749166+goodolLakshay@users.noreply.github.com>
2023-08-14 21:28:51 +0530

    added UMS autologin Script

---
+++ LPU UMS Auto Login.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         LPU UMS Auto Login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Autofill username and password on LPU UMS login page.
+// @author       You
+// @match        https://ums.lpu.in/lpuums/LoginNew.aspx
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Replace the following variables with your LPU UMS username and password
+    const username = 'Enter your username here';
+    const password = 'Enter yout password here';
+    
+    // Function to simulate a real click on an element
+    function simulateClick(element) {
+        const clickEvent = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true
+        });
+        element.dispatchEvent(clickEvent);
+    }
+
+    // Function to simulate a real click on the login button
+    function simulateLoginClick() {
+        const loginButton = document.getElementById('iBtnLogins');
+        simulateClick(loginButton);
+    }
+
+    // Wait for the login page to fully load before autofilling the login form
+    window.addEventListener('load', function() {
+        // Fill in the username field
+        const usernameField = document.getElementById('txtU');
+        usernameField.value = username;
+
+        // Fill in the password field
+        const passwordField = document.getElementById('TxtpwdAutoId_8767');
+        passwordField.value = password;
+
+        // Submit the form by clicking the login button
+        simulateLoginClick();
+    });
+})();
